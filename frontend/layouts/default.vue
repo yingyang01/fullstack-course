9cd20fa05376b5182fb6055b626697d86a60846d
notsu <hello@notsu.io>
2023-11-11 21:27:14 +0700

    feat(frontend): header

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,4 +1,7 @@
 <template>
+    <!-- TODO: 2. นำไปใช้งาน -->
+    <!-- <TheHeader /> -->
+
     <main class="bg-white max-w-screen-lg m-auto px-3">
         <slot />
     </main>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,29 +1,3 @@
 <template>
     หน้าแรก
-    <!-- TODO: หา Icon และนำมาใช้ -->
-    <!-- <BaseLink
-        variant="primary"
-        to="/portfolios/1"
-    >
-        <IconEnvelope class="inline-block w-3" />
-        <span>Primary Small</span>
-    </BaseLink> -->
-    <BaseButton
-        size="large"
-        variant="secondary"
-    >
-        Secondary
-    </BaseButton>
-    <BaseButton
-        size="large"
-        variant="danger"
-    >
-        Danger
-    </BaseButton>
-    <BaseButton
-        size="large"
-        variant="disabled"
-    >
-        Disabled
-    </BaseButton>
 </template>
