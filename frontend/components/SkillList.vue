b35d7fef6ed4bfb5db43070641891a0dcc77c2cf
notsu <hello@notsu.io>
2023-11-11 21:52:54 +0700

    feat(frontend): add skill list

---
+++ frontend/components/SkillList.vue
@@ -0,0 +1,36 @@
+<script setup>
+// TODO: 1. ออกแบบ Props ของ Component นี้
+// defineProps({
+//     skills: Array,
+//     canEdit: Boolean,
+// });
+
+// TODO: 2. รู้จัก Emit
+// defineEmits(['edit']);
+</script>
+
+<template>
+    <!-- TODO: 3. รู้จัก conditional rendering -->
+    <!-- TODO: 4. รู้จัก emits -->
+    <!-- <h2 class="text-xl font-bold mb-3 flex justify-between">
+        <span>Skills</span>
+        <BaseButton
+            v-if="canEdit"
+            size="small"
+            variant="secondary"
+            @click="$emit('edit')"
+        >
+            Edit
+        </BaseButton>
+    </h2>
+    <BaseBadgeList
+        v-if="skills.length > 0"
+        :badges="skills"
+    />
+    <span
+        v-else
+        class="text-grey"
+    >
+        None
+    </span> -->
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,20 @@
 <script setup>
-// TODO: 1. เติม useHead
-// useHead({
-//     title: 'Pichet Itngam\'s portfolios',
-//     meta: [
-//         { name: 'description', content: 'Pichet Itgnam\'s portfolios' },
-//     ],
-// })
+useHead({
+    title: 'Pichet Itngam\'s portfolios',
+    meta: [
+        { name: 'description', content: 'Pichet Itgnam\'s portfolios' },
+    ],
+})
 </script>
 
 <template>
-    <!-- TODO: 2. ใส่ placeholder สำหรับ แต่ละ section -->
-    <!-- <section class="mb-10"></section> -->
+    <section class="mb-10">
+        <!-- TODO: นำ Skill List มาใช้ -->
+    </section>
+    <section class="mb-10">
+        Experience List
+    </section>
+    <section class="mb-10">
+        Portfolio List
+    </section>
 </template>
