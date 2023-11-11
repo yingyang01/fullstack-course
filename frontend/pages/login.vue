6dbd164363adda770415fabe1398b008393a83cf
notsu <hello@notsu.io>
2023-11-12 00:42:30 +0700

    feat(frontend): pinia store

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,4 +1,7 @@
 <script setup>
+// TODO: 6. import มาใช้
+// import { useAuthStore } from '@/stores/auth';
+
 definePageMeta({
     layout: 'center',
 });
@@ -6,6 +9,9 @@ definePageMeta({
 useHead({
     title: 'Login',
 });
+
+// TODO: 7. init store
+// const store = useAuthStore();
 
 const onSubmit = async (values) => {
     console.log('values', values);
@@ -15,5 +21,16 @@ const onSubmit = async (values) => {
 <template>
     <main class="m-auto w-[300px]">
         <LoginForm @submit="onSubmit" />
+
+
+        <!-- TODO: 8. ลองใช้งาน -->
+        <!-- <p>{{ store.number }}</p>
+        <p>{{ store.multiplyByTwo }}</p>
+        <BaseButton
+            size="small"
+            @click="store.increase"
+        >
+            Increase
+        </BaseButton> -->
     </main>
 </template>
---
+++ frontend/stores/auth.js
@@ -0,0 +1,22 @@
+// TODO: 1. import pinira
+// import { defineStore } from 'pinia';
+
+// export const useAuthStore = defineStore('auth', () => {
+//     // TODO: 2. สร้าง reactive variable
+//     const number = ref(1);
+
+//     // TODO: 3. สร้าง computed
+//     const multiplyByTwo = computed(() => number.value * 2);
+
+//     // TODO: 4. สร้างฟังก์ชั่นในการ mutate reactive variable
+//     function increase() {
+//         number.value = number.value + 1;
+//     }
+
+//     // TODO: 5. return ทั้งหมดกับไป
+//     return {
+//         number,
+//         multiplyByTwo,
+//         increase,
+//     }
+// })
