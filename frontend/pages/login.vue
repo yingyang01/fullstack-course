680ed33734f615c0ac19b84c25ec54269dee9192
notsu <hello@notsu.io>
2023-11-11 23:19:34 +0700

    feat(frontend): login

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,8 +2,27 @@
 definePageMeta({
     layout: 'center',
 });
+
+// TODO: handle submit event
+// function onSubmit() {
+//     console.log('submitted!');
+// }
 </script>
 
 <template>
-    หน้า Login
+    <!-- TODO: 1. Form submit -->
+    <!-- <form @submit.prevent="onSubmit">
+        <input
+            class="border"
+            name="username"
+            placeholder="Username"
+        >
+        <input
+            type="password"
+            class="border"
+            name="password"
+            placeholder="Password"
+        >
+        <BaseButton type="submit">Login</BaseButton>
+    </form> -->
 </template>
